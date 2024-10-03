243a59d974ff0920508298724cff503427d445eb
Henrique da Silva <henriqueeehdss@gmail.com>
2024-10-03 12:21:15 +0100

    Add exercicio

---
+++ CalcularArea/calculararea.js
@@ -0,0 +1,16 @@
+// Calcular area do quadrado com numeros fixos
+
+let largura = 10;
+let altura = 20;
+let areaTotal = largura * altura;
+
+console.log( "A area do Quadrado é: " + areaTotal)
+
+
+// Calcular area do triangulo
+
+let base = 10
+let alturaTriangulo = 20
+let areaTriangulo = (base*alturaTriangulo)/2
+
+console.log("A area do Triangulo é: " + areaTriangulo)
